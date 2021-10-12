d3dfa8e3aa5ae9d6b538d5775e3f2fe3eea16bc1
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-10-12 17:06:49 +0700

    cars

--- src/cars/Cars.java
+++ src/cars/Cars.java
@@ -1,4 +1,13 @@
 package cars;
 
 public class Cars {
+    private String name;
+    private String engine;
+    public static int numberOfCars;
+
+    public Cars(String name, String engine) {
+        this.name = name;
+        this.engine = engine;
+        numberOfCars ++;
+    }
 }
--- src/cars/TestStaticProperty.java
+++ src/cars/TestStaticProperty.java
@@ -1,4 +1,10 @@
 package cars;
 
 public class TestStaticProperty {
+    public static void main(String[] args) {
+        Cars car1 = new Cars("Mazda 3","Skyactiv 3");
+        System.out.println(Cars.numberOfCars);
+        Cars car2 = new Cars("Mazda 6","Skyactiv 6");
+        System.out.println(Cars.numberOfCars);
+    }
 }
